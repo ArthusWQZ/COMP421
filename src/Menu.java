a5865efe30fd2ca8cb1ffcf3468cda88a375ee88
wzuo514 <48163620+wzuo514@users.noreply.github.com>
2024-03-17 19:15:39 -0400

    Update Menu.java
    add in basic menu
    placeholders for functions

--- src/Menu.java
+++ src/Menu.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
 
+public class Menu {
+  public static void main(String[] args) {
+    System.out.println("1. Create a new session \n");
+    System.out.println("2. Delete unsold tickets from a session \n");
+    System.out.println("3. Add a new movie \n");
+    System.out.println("4. Remove session \n");
+    System.out.println("5. Assign a ticket to a customer \n");
+    System.out.println("6. Increase consummables price \n");
+    System.out.println("7. Quit");
+
+
+    String option = null;
+    Scanner input = new Scanner(System.in);
+    do {
+      option = scan.nextLine();
+      switch (option) {
+        case "1":
+          break;
+        case "2":
+          break;
+        case "3":
+          break;
+        case "4":
+          break;
+        case "5":
+          break;
+        case "6":
+          break;
+      }
+    while (!option.equals("7"))
+    }
+  }
+}
