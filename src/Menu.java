de65226563fb66db1adc8ee623eff0e65aca0b70
ArthusWQZ <arthus.wauquiez@neuf.fr>
2024-03-18 13:40:40 -0400

    Minor changes

--- src/Menu.java
+++ src/Menu.java
@@ -6,13 +6,15 @@ public class Menu {
     Scanner input = new Scanner(System.in);
 
     do {
+      System.out.println("=========CineFlex Admin Menu=========");
       System.out.println("1. Create a new session");
       System.out.println("2. Delete unsold tickets from a session");
       System.out.println("3. Add a new movie");
       System.out.println("4. Remove session");
       System.out.println("5. Assign a ticket to a customer");
       System.out.println("6. Increase consumables price");
-      System.out.println("7. Quit");
+      System.out.println("7. Quit\n");
+      System.out.print("> ");
       option = input.nextLine();
       switch (option) {
         case "1":
