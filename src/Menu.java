6205220aacbe6e89eff52594d3829b5e32475f65
Xin Yi Du <71841286+xinyidu@users.noreply.github.com>
2024-03-18 17:25:53 -0400

    Add files via upload

---
+++ src/Menu.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.sql.*;
+
+public class Menu {
+
+  Connection aCon;
+  Statement aStatement;
+
+  public Menu(Connection pCon, Statement pStatement)
+  {
+    aCon = pCon;
+    aStatement = pStatement;
+  }
+
+  public void enterMenuLoop() throws SQLException {
+    String option;
+    Scanner input = new Scanner(System.in);
+
+    do {
+      System.out.println("=========CineFlex Admin Menu=========");
+      System.out.println("1. Create a new session");
+      System.out.println("2. Delete unsold tickets from a session");
+      System.out.println("3. Add a new movie");
+      System.out.println("4. Remove session");
+      System.out.println("5. Assign a ticket to a customer");
+      System.out.println("6. Increase consumables price");
+      System.out.println("7. Quit\n");
+      System.out.print("> ");
+      option = input.nextLine();
+      switch (option) {
+        case "1":
+          break;
+        case "2":
+          Integer sessionID = input.nextInt();
+          DeleteUnsoldTickets.execute(aStatement, sessionID);
+          break;
+        case "3":
+          break;
+        case "4":
+          break;
+        case "5":
+          break;
+        case "6":
+          break;
+      }
+
+    } while (!option.equals("7"));
+  }
+}
