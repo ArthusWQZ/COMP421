69a9e10e575e548357979f7f6bdbd3caf9dd59a0
mill-gai <pornratmill@gmail.com>
2024-03-19 19:48:51 -0400

    delete unuse files

--- src/QueryString.java
+++
@@ -1,6 +0,0 @@
-public class QueryString {
-    public static String insertSessions = "INSERT INTO sessions VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
-    public static String selectMaxValue = "SELECT MAX(colName) FROM tableName";
-
-
-}
